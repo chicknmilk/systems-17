c9b6277c4a554bb34aa25fb8f4f40845c8ef097b
Aaron Hsu <aaronhsu4606@gmail.com>
2021-12-07 00:50:10 -0500

    run computer

--- write.c
+++ write.c
@@ -41,7 +41,7 @@ int main() {
   lseek(fd, 0, SEEK_END);
   write(fd, new_data, strlen(new_data));
   *data = strlen(new_data);
-  printf("wrote "%s" to file\n", new_data);
+  printf("wrote %s to file\n", new_data);
 
 
   shmdt(data);
