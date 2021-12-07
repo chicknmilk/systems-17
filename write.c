e5b6fda1fb5e33159e4951e9a624b815b21b14c0
Aaron Hsu <aaronhsu4606@gmail.com>
2021-12-06 22:59:41 -0500

    run computer

--- write.c
+++ write.c
@@ -3,7 +3,7 @@
 int main() {
   
   // GRAB THE SEMAPHORE!
-  int semd = semget(SEM_KEY, 0, 0);
+  int semd = semget(SEM_KEY, 1, 0);
 
   struct sembuf sb;
   sb.sem_num = 0;
@@ -17,18 +17,16 @@ int main() {
   char *data = shmat(shmd, 0, 0);
 
 
-  // open file and print out the last line
-  int file = open("tmp.txt", O_RDWR | O_APPEND);
+  // open file
+  int fd = open("temp.txt", O_RDWR | O_APPEND);
 
-  // print out the last line of the file
-  char *line = malloc(sizeof(char) * 100);
-  int i = 0;
-  while (data[i] != '\n') {
-    line[i] = data[i];
-    i++;
-  }
-  line[i] = '\0';
-  printf("%s\n", line);
-  
+  // use lseek to move to the end of the file
+  lseek(fd, 0, SEEK_END);
+  // read the last line in the file
+  char *last = data + strlen(data) - 1;
+
+  //print the last line
+  printf("%s\n", last);
+
 
 }
