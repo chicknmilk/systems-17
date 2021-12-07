72c2e6d1692c4e7306f58083340c50a8e6780504
Aaron Hsu <aaronhsu4606@gmail.com>
2021-12-06 22:43:44 -0500

    run computer

---
+++ write.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define SEMKEY 24602
+#define SHMKEY 24603
